7b3d9eac5812b32cde02870065b21f9a6067b8be
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-04-22 16:33:43 +0000

    Automatically backed up by Learn

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -2,10 +2,19 @@
 import React, { Component } from 'react'
 
 class BandInput extends Component {
+
+  this.state = {
+    text: ''
+  }
+
   render() {
     return(
       <div>
         Band Input
+        <form>
+          <input type="text" />
+          <input type="submit" />
+        </form>
       </div>
     )
   }
